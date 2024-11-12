42920a4c85e9faad3afeb9c283c8ced10ac6a35f
khoaedu <trannguyenanhkhoa0104@gmail.com>
2024-11-12 22:09:00 +0700

    test

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,6 +15,7 @@ const Projects = () => {
     }
   return (
     <div className="container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden" id="Projects">
+        {/* under next-row may be: text-underline-position: under; */}
       <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">Projects <span className="underline underline-offset-4 decoration-1 under font-light">Completed</span></h1>
       <p className="text-gray-500 max-w-80 text-center mb-8 mx-auto">Crafting Spaces, Building Legacies-Explore Our Portfolio</p>
       
