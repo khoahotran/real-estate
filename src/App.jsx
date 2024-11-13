31e81e7760cf0e27bd01e0faf613b66239cb5a6b
khoaedu <trannguyenanhkhoa0104@gmail.com>
2024-11-13 11:11:38 +0700

    2h22m22s

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import Header from './components/Header'
 import Projects from './components/Projects'
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Footer from './components/Footer';
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
+      <ToastContainer />
       <Header/>
       <About/>
       <Projects/>
       <Testimonials/>
       <Contact/>
+      <Footer />
     </div>
   )
 }
